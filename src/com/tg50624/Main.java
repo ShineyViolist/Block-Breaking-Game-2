b8904425f73e36a6c296a96288f8a4c52b4f575f
ShineyViolist <65990530+ShineyViolist@users.noreply.github.com>
2021-01-09 22:20:24 -0500

    Update Main.java

--- src/com/tg50624/Main.java
+++ src/com/tg50624/Main.java
@@ -1,19 +1,15 @@
 package com.tg50624;
+
 import javax.swing.*;
-import java.awt.*;
 
-public class Main {
+public class Main{
 
-    public static void main(String[] args){
-	    JFrame frame = new JFrame("Block breaking game");
-
-
-		frame.getContentPane().setBackground(Color.PINK);
+    public static void main(String[] args) {
+	    JFrame frame = new JFrame("Block Breaking Game");
 		BlockPanel panel = new BlockPanel();
 		frame.getContentPane().add(panel);
-
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+	    frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+	    frame.setVisible(true);
 		frame.setResizable(false);
 		frame.setSize(900,900);
     }
